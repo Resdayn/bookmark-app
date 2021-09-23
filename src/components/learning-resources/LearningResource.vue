3c8c00a2df0ca7d57c0091321791dcb666af58c6
Juan González <jgonzalez@naver.com>
2021-09-23 12:14:59 +0900

    Styled

--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -18,3 +18,39 @@ export default {
     props: ['title', 'description', 'link']
 }
 </script>
+
+<style scoped>
+li {
+  margin: 1rem auto;
+  padding: 0.3rem 1rem;
+  max-width: 40rem;
+  border-radius: 25px;
+  color: #C5CAD8;
+  background-color: #3677A4;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: #ce5c00;
+}
+
+a:hover,
+a:active {
+  color: #c89300;
+}
+</style>
