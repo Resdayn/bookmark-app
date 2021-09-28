35ef241404812621ca99a4c67e568a4df29b22cf
Juan González <jgonzalez@naver.com>
2021-09-28 16:42:16 +0900

    added listener for the backdrop close event

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,5 +1,5 @@
 <template>
-  <base-dialog v-if="inputIsInvalid" title="Invalid Input">
+  <base-dialog v-if="inputIsInvalid" title="Invalid Input" @close="confirmError">
     <template #default>
       <p>At least one input value is invalid.</p>
       <p>Please check all input and make sure you enter at least a few characters</p>
