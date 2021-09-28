b68bb334936b535e77f6a96be167dd2049d7a9cb
Juan González <jgonzalez@naver.com>
2021-09-28 16:20:20 +0900

    BaseDialog fixed

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import App from "./App.vue"
 import BaseCard from "./components/UI/BaseCard.vue"
 import BaseButton from "./components/UI/BaseButton.vue"
-import BaseButton from "./components/UI/BaseDialog.vue"
+import BaseDialog from "./components/UI/BaseDialog.vue"
 
 const app = createApp(App);
 
