5b6658a911d8a52e9c2e34ff227696b2b268af73
Juan González <jgonzalez@naver.com>
2021-10-15 10:48:27 +0900

    created

---
+++ src/components/UI/LoadingSpinner.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="lds-dual-ring"></div>
+</template>
+
+<style scoped>
+.lds-dual-ring {
+  display: inline-block;
+  width: 5px;
+  height: 5px;
+  margin-left: 1rem;
+  padding-bottom: .6rem ;
+}
+.lds-dual-ring:after {
+  content: " ";
+  display: block;
+  width: 6px;
+  height: 6px;
+  border-radius: 50%;
+  border: 3px solid #fff;
+  border-color: #fff transparent #fff transparent;
+  animation: lds-dual-ring 1.2s linear infinite;
+}
+@keyframes lds-dual-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
